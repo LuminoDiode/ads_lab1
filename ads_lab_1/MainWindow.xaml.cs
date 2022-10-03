96a2b3be9ec13341f0bb8a0a09e6edfd2c4d3c8e
Rodion R <65biso2@gmail.com>
2022-10-03 22:01:31 +0300

    code changes

--- ads_lab_1/MainWindow.xaml.cs
+++ ads_lab_1/MainWindow.xaml.cs
@@ -14,6 +14,13 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
 
 namespace ads_lab_1
 {
@@ -27,16 +34,7 @@ namespace ads_lab_1
 			InitializeComponent();
 			this.InputRTB.AppendText("int a = 1;\r\nint b = 2;\r\nint c =3;\r\ndo {\r\na/b*c} \r\nwhile(a>b)\r\n;\r\nfor(int i =0; i<2;i++) a+=2;\r\nint a = 1;\r\nint b = 2;\r\nint c =3;\r\ndo {a/m*c} while(a>b);\r\nfor(int i =0; i<2;i++) a+=2;\r\ndo {a/m*c} while(a>b);");
 		}
-
-		private void Button_Click(object sender, RoutedEventArgs e)
-		{
-			ClaimMatcher matcher = new ClaimMatcher();
-
-			OutputRTB.Document.Blocks.Clear();
-			OutputRTB.AppendText(string.Join("\n\n", matcher.matches(new TextRange(InputRTB.Document.ContentStart, InputRTB.Document.ContentEnd).Text)));
-		}
-
-		private void Button_Click_1(object sender, RoutedEventArgs e)
+		private void LoadFileButton_Click(object sender, RoutedEventArgs e)
 		{
 			OpenFileDialog d = new();
 			d.Filter = @"Text files (*.txt)|*.txt|All files (*.*)|*.*";
@@ -47,6 +45,91 @@ namespace ads_lab_1
 				InputRTB.Document.Blocks.Clear();
 				InputRTB.AppendText(File.ReadAllText(d.FileName));
 			}
+		}
+
+		private void Lab1_Click(object sender, RoutedEventArgs e)
+		{
+			ClaimMatcher matcher = new ClaimMatcher();
+
+			OutputRTB.Document.Blocks.Clear();
+			OutputRTB.AppendText(string.Join("\n\n", matcher.matches(new TextRange(InputRTB.Document.ContentStart, InputRTB.Document.ContentEnd).Text)));
+		}
+		private void Lab2_Click(object sender, RoutedEventArgs e)
+		{
+			// 18. целой длинной константой. Модуль > 2^31
+			var found = new Regex(@"(?<=(\s|\A)[#]define\s+MYCONST\s+)\d+").Match(new TextRange(InputRTB.Document.ContentStart, InputRTB.Document.ContentEnd).Text).Value;
+			if (!string.IsNullOrEmpty(found) && long.TryParse(found, out var parsed) && Math.Abs(parsed) > Math.Pow(2, 31))
+			{
+				MessageBox.Show($"Объявлена длинная целая константа MYCONST со значением {parsed}");
+			}
+			else
+			{
+				MessageBox.Show("Объявления длинной константы MYCONST не найдено.");
+			}
+
+		}
+		private void Lab3_Click(object sender, RoutedEventArgs e)
+		{
+			// 18. целой длинной константой. Модуль > 2^31
+			var found = new Regex(@"(?<=(\s|\A)[#]define\s+MYCONST\s+)\d+").Match(new TextRange(InputRTB.Document.ContentStart, InputRTB.Document.ContentEnd).Text).Value;
+			if (!string.IsNullOrEmpty(found) && long.TryParse(found, out var parsed) && Math.Abs(parsed) > Math.Pow(2, 31))
+			{
+				MessageBox.Show($"Объявлена длинная целая константа MYCONST со значением {parsed}");
+			}
+			else
+			{
+				MessageBox.Show("Объявления длинной константы MYCONST не найдено.");
+			}
+		}
+		private void Lab4_Click(object sender, RoutedEventArgs e)
+		{
+			var text = new TextRange(InputRTB.Document.ContentStart, InputRTB.Document.ContentEnd).Text;
+			var canBeShortenedRegex = new Regex(@"(?<=(\w*?))[ауоыэяюёие]\w+?");
+			// Вариант 2 – для чётных номеров.
+			// В тексте сократить все слова после согласных букв, расположенных за первой гласной буквой или последовательными гласными буквами.
+			// Знаки препинания должны остаться без изменений.
+			StringBuilder sb = new StringBuilder(text.Length * 2 / 3);
+
+			var Vowels = "ауоыэяюёие".ToCharArray();
+			int currentWordStart = 0;
+			bool haveSeenVowel = false;
+			bool dotAdded = false;
+			for (int i = 0; i < text.Length; i++)
+			{
+				if (char.IsPunctuation(text[i])) // пунктуация сохранена
+				{
+					sb.Append(text[i]);
+				}
+				else if (char.IsWhiteSpace(text[i])) // пробелы сохранены и обнуляют флаги
+				{
+					haveSeenVowel = false;
+					dotAdded = false;
+					sb.Append(text[i]);
+				}
+				else if (!dotAdded) // если точка уже есть, то только пробелы и пунктуация
+				{
+					if (Vowels.Contains(text[i])) // гласная?
+					{
+						if (haveSeenVowel && !(i>0 && Vowels.Contains(text[i-1]))) // не первая гласная и не подряд? оборвать слово.
+						{
+							sb.Append('.');
+							dotAdded = true;
+						}
+						else // первая гласная? - продолжить далее.
+						{
+							sb.Append(text[i]);
+							haveSeenVowel = true;
+						}
+					}
+					else
+					{
+						sb.Append(text[i]);
+					}
+				}
+			}
+
+			OutputRTB.Document.Blocks.Clear();
+			OutputRTB.AppendText(sb.ToString());
 		}
 	}
 }
