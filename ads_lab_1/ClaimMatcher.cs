741ed0365b725d219e3c3ea492bb589b4a697e93
Rodion R <65biso2@gmail.com>
2022-10-03 18:54:03 +0300

    Add project files.

---
+++ ads_lab_1/ClaimMatcher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ads_lab_1
+{
+	// Оператор цикла do while и знаки математических операций / и *.
+	internal class ClaimMatcher
+	{
+		private List<Regex> claimMatcher=new();
+
+		public ClaimMatcher()
+		{
+			claimMatcher.Add(new Regex(@"(?<=(\s|\A))do[\s]*[{]\s*.+\s*[}][\s]*while[(].+[)]\s*[;]"));
+			claimMatcher.Add(new Regex(@"[^/][/][^/]"));
+			claimMatcher.Add(new Regex(@"[^/][/][^/]"));
+		}
+
+		public IEnumerable<string> matches(string code)
+		{
+			var matches = claimMatcher[0].Matches(code).Select(x => x.Value);
+			for(int i =1;i<claimMatcher.Count-1;i++)
+			{
+				matches = matches.Where(x => claimMatcher[i].IsMatch(x));
+			}
+			return matches;
+		}
+	}
+}
---
+++ ads_lab_1/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ads_lab_1
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			this.InputRTB.AppendText("int a = 1;\r\nint b = 2;\r\nint c =3;\r\ndo {\r\na/b*c} \r\nwhile(a>b)\r\n;\r\nfor(int i =0; i<2;i++) a+=2;\r\nint a = 1;\r\nint b = 2;\r\nint c =3;\r\ndo {a/m*c} while(a>b);\r\nfor(int i =0; i<2;i++) a+=2;\r\ndo {a/m*c} while(a>b);");
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			ClaimMatcher matcher = new ClaimMatcher();
+
+			OutputRTB.Document.Blocks.Clear();
+			OutputRTB.AppendText(string.Join("\n\n", matcher.matches(new TextRange(InputRTB.Document.ContentStart, InputRTB.Document.ContentEnd).Text)));
+		}
+
+		private void Button_Click_1(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog d = new();
+			d.Filter = @"Text files (*.txt)|*.txt|All files (*.*)|*.*";
+			d.ShowDialog();
+
+			if (!string.IsNullOrEmpty(d.FileName))
+			{
+				InputRTB.Document.Blocks.Clear();
+				InputRTB.AppendText(File.ReadAllText(d.FileName));
+			}
+		}
+	}
+}
