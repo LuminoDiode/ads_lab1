51186c0e9fcf9b913e641d12f073b8c6809de319
Rodion R <65biso2@gmail.com>
2022-11-07 23:17:51 +0300

    just adding some random stuff to the repo

---
+++ Tests/VirtualString.cs
@@ -0,0 +1,184 @@
+﻿using ads_lab_1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Tests
+{
+	public class VirtualStringTests
+	{
+		private readonly ITestOutputHelper output;
+		private readonly string testString;
+		private readonly LazyString testVirtual;
+
+		public VirtualStringTests(ITestOutputHelper output)
+		{
+			this.output = output;
+			testString = "1234567890";
+			testVirtual = (LazyString)testString;
+		}
+
+		[Fact]
+		public void CanCreateInstance()
+		{
+			string s = "123";
+			var vs = (LazyString)(s);
+			Assert.Equal(s, vs.ToString());
+
+			vs = new LazyString(s, 0, s.Length);
+			Assert.Equal(s, vs.ToString());
+
+			vs = new LazyString(vs, 0, s.Length);
+			Assert.Equal(s, vs.ToString());
+		}
+
+		[Fact]
+		public void CanSubstring()
+		{
+			int startIndex = 1, count = 9;
+
+			var s = "1234567890";
+			var ss = s.Substring(startIndex, count);
+
+			var vs1 = new LazyString(s, startIndex, count);
+			var vs2 = ((LazyString)s).Substring(startIndex, count); ;
+
+			Assert.Equal(ss, vs1.ToString());
+			Assert.Equal(ss, vs2.ToString());
+
+			var rnd = new Random(DateTime.UtcNow.Millisecond);
+			for (int i = 0; i < 1000; i++)
+			{
+				s = long.MaxValue.ToString();
+				startIndex = rnd.Next(s.Length);
+				count = rnd.Next(0,s.Length - startIndex + 1);
+
+				Assert.Equal(s.Substring(startIndex, count), ((LazyString)s).Substring(startIndex, count));
+			}
+		}
+
+		[Fact]
+		public void CanInsert()
+		{
+			var s = "1234567890";
+			var vs = (LazyString)(s);
+
+			Assert.Equal(s.Insert(0, s), vs.Insert(0, s));
+			Assert.Equal(s.Insert(s.Length, s), vs.Insert(s.Length, s));
+			Assert.Equal(s.Insert(s.Length / 2, s), vs.Insert(s.Length / 2, s));
+		}
+
+		[Fact]
+		public void CanRemove()
+		{
+			var s = "1234567890";
+			var vs = (LazyString)(s);
+
+			Assert.Equal(s.Remove(0, s.Length), vs.Remove(0, s.Length));
+
+			//Assert.Equal(
+			//	s.Remove((int)Math.Floor((s.Length - 1) / 2d), (int)Math.Ceiling(s.Length / 2d)),
+			//	vs.Remove((int)Math.Floor((s.Length - 1) / 2d), (int)Math.Ceiling(s.Length / 2d)));
+
+			//Assert.Equal(s.Remove(s.Length - 1, 0), vs.Remove(s.Length - 1, 0));
+
+			//Assert.Equal(s.Remove(1, s.Length - 1), vs.Remove(1, s.Length - 1));
+			//Assert.Equal(s.Remove(3, s.Length - 3), vs.Remove(3, s.Length - 3));
+		}
+
+		[Fact]
+		public void CanDoMultipleOperations()
+		{
+			int numberOfIterations = 1000;
+			var rand = new Random(DateTime.UtcNow.Millisecond);
+
+			var originalString = (int.MaxValue / rand.Next(1, 10)).ToString();
+			var virtualString = (LazyString)originalString;
+
+			for (int i = 0; i < numberOfIterations; i++)
+			{
+				// insert ?
+				if (rand.Next(0, 2) == 1)
+				{
+					var insertionIndex = rand.Next(0, virtualString.Length);
+					var insertionValue = rand.Next(0, int.MaxValue).ToString();
+
+					virtualString = virtualString.Insert(insertionIndex, insertionValue);
+					originalString = originalString.Insert(insertionIndex, insertionValue);
+				}
+
+				// remove ?
+				if (rand.Next(0, 2) == 1)
+				{
+					var removeIndex = rand.Next(0, virtualString.Length);
+					var removeCount = rand.Next(0, virtualString.Length - removeIndex);
+
+					virtualString = virtualString.Remove(removeIndex, removeCount);
+					originalString = originalString.Remove(removeIndex, removeCount);
+				}
+
+				output.WriteLine(originalString);
+			}
+
+			Assert.Equal(originalString, virtualString);
+		}
+
+		[Fact]
+		public void CanGetIndexOf()
+		{
+			var rand = new Random(DateTime.UtcNow.Millisecond);
+			var originalString = (int.MaxValue / rand.Next(1, 10)).ToString();
+			var virtualString = (LazyString)originalString;
+			char c;
+
+			c = originalString[0];
+			Assert.Equal(originalString.IndexOf(c), virtualString.IndexOf(c));
+
+			c = originalString[originalString.Length / 2];
+			Assert.Equal(originalString.IndexOf(c), virtualString.IndexOf(c));
+
+			c = originalString[originalString.Length - 1];
+			Assert.Equal(originalString.IndexOf(c), virtualString.IndexOf(c));
+		}
+
+		[Fact]
+		public void CanUseStartsWith()
+		{
+			var rand = new Random(DateTime.UtcNow.Millisecond);
+			var originalString = (int.MaxValue / rand.Next(1, 10)).ToString();
+			var virtualString = (LazyString)originalString;
+			string s;
+
+			s = originalString.Substring(0);
+			Assert.Equal(originalString.StartsWith(s), virtualString.StartsWith(s));
+
+			s = originalString.Substring(originalString.Length / 2);
+			Assert.Equal(originalString.StartsWith(s), virtualString.StartsWith(s));
+
+			s = originalString.Substring(originalString.Length);
+			Assert.Equal(originalString.StartsWith(s), virtualString.StartsWith(s));
+		}
+
+
+		[Fact]
+		public void CanUseEndsWith()
+		{
+			var rand = new Random(DateTime.UtcNow.Millisecond);
+			var originalString = (int.MaxValue / rand.Next(1, 10)).ToString();
+			var virtualString = (LazyString)originalString;
+			string s;
+
+			s = originalString.Substring(0);
+			Assert.Equal(originalString.EndsWith(s), virtualString.EndsWith(s));
+
+			s = originalString.Substring(originalString.Length / 2, originalString.Length - (originalString.Length / 2));
+			Assert.Equal(originalString.EndsWith(s), virtualString.EndsWith(s));
+
+			s = originalString.Substring(originalString.Length);
+			Assert.Equal(originalString.EndsWith(s), virtualString.EndsWith(s));
+		}
+	}
+}
---
+++ ads_lab_1/LazyString.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ads_lab_1
+{
+	public class LazyString :
+		IEnumerable<char>,
+		IEquatable<LazyString>,
+		IEquatable<string>,
+		IComparable<LazyString>,
+		IComparable<string>
+	{
+		protected readonly IEnumerable<char> _originString;
+		protected readonly int _startIndex;
+		protected readonly int _count;
+		public int Length => _count;
+
+		protected readonly LazyString _inserted = null!;
+		protected readonly int _insertedIndex = -1;
+
+		protected readonly int _removedStart = -1;
+		protected readonly int _removedCount = int.MaxValue;
+
+		protected int getMaxIndex() => Math.Max(0, 
+			Math.Max(0,_count-1)
+			+ (_insertedIndex == -1 ? 0 : _inserted.Length)
+			- (_removedStart == -1 ? 0 : _removedCount));
+		protected bool failsCheck() => (_startIndex < 0
+				|| _count < 0);
+
+		public LazyString(LazyString s, int startIndex, int count)
+		{
+			_originString = s;
+			_startIndex = startIndex;
+			_count = count;
+
+			if (failsCheck() || (s.Length - startIndex) < count)
+				throw new ArgumentOutOfRangeException();
+		}
+		public LazyString(string s, int startIndex, int count)
+		{
+			_originString = s;
+			_startIndex = startIndex;
+			_count = count;
+
+			if (failsCheck() || (s.Length-startIndex)<count)
+				throw new ArgumentOutOfRangeException();
+		}
+		protected LazyString(LazyString s, int startIndex, int count, LazyString inserted, int insertedIndex)
+		{
+			_inserted = inserted;
+			_insertedIndex = insertedIndex;
+			_originString = s;
+			_startIndex = startIndex;
+			_count = count;
+
+			if (failsCheck() || (s.Length - startIndex + inserted.Length) < count)
+				throw new ArgumentOutOfRangeException();
+		}
+		protected LazyString(LazyString s, int startIndex, int count, int removedStart, int removedCount)
+		{
+			_removedStart = removedStart;
+			_removedCount = removedCount;
+			_originString = s;
+			_startIndex = startIndex;
+			_count = count;
+
+			if (failsCheck() || (s.Length - startIndex - removedCount) < count)
+				throw new ArgumentOutOfRangeException();
+		}
+
+		public static implicit operator LazyString(string s)
+		{
+			return new LazyString(s, 0, s.Length);
+		}
+		public static explicit operator string(LazyString s)
+		{
+			return s.ToString();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return this.GetEnumerator();
+		}
+		public IEnumerator<char> GetEnumerator()
+		{
+			var enumer = _originString.GetEnumerator();
+
+			for (int i = 0; i < _startIndex; i++)
+			{
+				enumer.MoveNext();
+			}
+
+			for (int currentIndex = 0; currentIndex < _count;)
+			{
+				if (currentIndex == _insertedIndex)
+				{
+					var enumerInserted = _inserted.GetEnumerator();
+					while (enumerInserted.MoveNext())
+					{
+						yield return enumerInserted.Current;
+						currentIndex++;
+					}
+				}
+				if (currentIndex == _removedStart)
+				{
+					for (int r = 0; r < _removedCount; r++)
+					{
+						enumer.MoveNext();
+					}
+				}
+
+				if (enumer.MoveNext())
+				{
+					yield return enumer.Current;
+					currentIndex++;
+				}
+#if DEBUG
+				else
+				{
+					if (currentIndex < _count)
+					{
+						throw new IndexOutOfRangeException("Collection should reach its end here.");
+					}
+				}
+#endif
+			}
+
+			yield break;
+		}
+
+		public LazyString Substring(int start, int count)
+		{
+			return new LazyString(this, start, count);
+		}
+
+		public bool EndsWith(LazyString end)
+		{
+			return this._originString.Skip(_startIndex).Take(_count).TakeLast(end.Length).SequenceEqual(end);
+		}
+		public bool EndsWith(char c)
+		{
+			return this._originString.Skip(_startIndex).Take(_count).LastOrDefault().Equals(c);
+		}
+		public bool StartsWith(LazyString start)
+		{
+			return this._originString.Skip(_startIndex).Take(_count).Take(start.Length).SequenceEqual(start);
+		}
+		public bool StartsWith(char c)
+		{
+			return this._originString.Skip(_startIndex).Take(_count).FirstOrDefault().Equals(c);
+		}
+		public int IndexOf(char c)
+		{
+			var enumerThis = this.GetEnumerator();
+
+			for (int i = 0; enumerThis.MoveNext(); i++)
+			{
+				if (enumerThis.Current.Equals(c)) return i;
+			}
+
+			return -1;
+		}
+
+
+		public LazyString Insert(int index, LazyString inserted)
+		{
+			return new LazyString(this, 0, this._count + inserted.Length, inserted, index);
+		}
+
+		public LazyString Remove(int startIndex, int count)
+		{
+			return new LazyString(this, 0, this._count - count, startIndex, count);
+		}
+
+		public override string ToString()
+		{
+			var result = new char[this.Length];
+			var enumer = this.GetEnumerator();
+			for (int i = 0; i < this.Length; i++)
+			{
+#if RELEASE
+				enumer.MoveNext();
+#endif
+#if DEBUG
+				if (!enumer.MoveNext())
+				{
+					throw new IndexOutOfRangeException("Collection should not reach its end here.");
+				}
+#endif
+				result[i] = enumer.Current;
+			}
+
+			return new string(result);
+		}
+
+		bool IEquatable<LazyString>.Equals(LazyString? other)
+		{
+			if (other is null) return this is null ? true : false;
+
+			return other.SequenceEqual(this._originString);
+		}
+		bool IEquatable<string>.Equals(string? other)
+		{
+			if (other is null) return this is null ? true : false;
+
+			return other.SequenceEqual(this._originString);
+		}
+
+		int IComparable<LazyString>.CompareTo(ads_lab_1.LazyString? other)
+		{
+			if (other is null) return 1;
+			var enumerThis = this.GetEnumerator();
+			var enumerOther = other.GetEnumerator();
+
+			while (true)
+			{
+				var cThis = enumerThis.MoveNext();
+				var cOther = enumerOther.MoveNext();
+
+				if (cThis != cOther)
+				{
+					return cThis == false ? -1 : 1;
+				}
+
+				if (enumerThis.Current != enumerOther.Current)
+				{
+					return enumerThis.Current - enumerOther.Current;
+				}
+			}
+		}
+		int IComparable<string>.CompareTo(string? other)
+		{
+			if (other is null) return 1;
+			return (this as IComparable<LazyString>).CompareTo((LazyString)other);
+		}
+
+	}
+}
